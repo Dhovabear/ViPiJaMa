006bc2e1a03e4aa468f04fc325571949c83f0e1d
Dhovabear <43112786+dhovabear@users.noreply.github.com>
2019-11-18 21:22:21 +0100

    Ajout(Shop): ajout du shop

---
+++ src/Scenes/Shop.java
@@ -0,0 +1,116 @@
+package Scenes;
+
+import Exceptions.ValeurNegatifException;
+import Exceptions.ValeurTropGrandeException;
+import Model.Color;
+import Model.Face;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Shop implements Initializable {
+
+    @FXML
+    ComboBox<Integer> prix;
+
+    @FXML
+    VBox listArticles;
+
+    HashMap<Integer,List<Face>> magasin;
+
+
+
+    public void Buy(ActionEvent actionEvent) {
+        HBox b = (HBox)((Node)actionEvent.getSource()).getParent();
+        int idToBuy = Integer.parseInt(b.getId());
+        System.out.println("idTo>Buy="+idToBuy);
+        int currentPrix = prix.getValue();
+        //on donne le truc au joueur
+        magasin.get(currentPrix).remove(magasin.get(currentPrix).get(idToBuy));
+        updateArt(actionEvent);
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("protoooot");
+        magasin = new HashMap<Integer, List<Face>>();
+        //TODO: ajouter les articles avec la liste et l'associer a un prix
+        List<Face> list1 = new ArrayList<Face>();
+        try {
+            list1.add(new Face(5, Color.Or));
+            list1.add(new Face(6, Color.Lunaire));
+            list1.add(new Face(4, Color.Gloire));
+            list1.add(new Face(2, Color.Solaire));
+
+        } catch (Exception e) {}
+        magasin.put(2,list1);
+        List<Face> list2 = new ArrayList<Face>();
+        try {
+            list2.add(new Face(1, Color.Solaire));
+            list2.add(new Face(6, Color.Lunaire));
+            list2.add(new Face(1, Color.Gloire));
+            list2.add(new Face(2, Color.Solaire));
+            list2.add(new Face(2, Color.Gloire));
+            list2.add(new Face(2, Color.Solaire));
+
+        } catch (Exception e) {}
+        magasin.put(3,list2);
+        magasin.put(4,new ArrayList<>());
+        magasin.put(5,new ArrayList<>());
+        magasin.put(6,new ArrayList<>());
+        magasin.put(8,new ArrayList<>());
+        magasin.put(12,new ArrayList<>());
+    }
+
+    public void updateArt(ActionEvent actionEvent) {
+        int prixn = prix.getValue();
+        listArticles.getChildren().clear();
+        if(magasin.get(prixn).isEmpty() || !magasin.containsKey(prixn)){
+            Label l2 = new Label();
+            l2.setStyle("-fx-background-color: red");
+            l2.setText("Aucun article disponible pour le moment");
+            listArticles.getChildren().add(l2);
+            return;
+        }
+        for (Face f : magasin.get(prixn)){
+
+            Label l = new Label();
+            l.setText(f.getColor().toString() +"\t"+ f.getValeur());
+            l.setLineSpacing(100);
+            l.setMinWidth(200);
+            l.setTextAlignment(TextAlignment.CENTER);
+
+            Button bt = new Button();
+            bt.setText("Buy");
+            bt.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    Buy(actionEvent);
+                }
+            });
+
+            HBox hb = new HBox();
+            hb.setStyle("-fx-background-color: beige");
+            hb.getChildren().add(l);
+            hb.getChildren().add(bt);
+            hb.setId(Integer.toString(magasin.get(prixn).indexOf(f)));
+
+            listArticles.getChildren().add(hb);
+        }
+    }
+}
