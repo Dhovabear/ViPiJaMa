1b3a2ce231f03cbd077d667285a9561236714409
Rémi Jeanneret <remj70@gmail.com>
2019-12-03 14:26:08 +0100

    cartes de ses morts

---
+++ src/Model/Carte.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Carte {
+    String image;
+    int prixLunaire, prixSolaire;
+
+    public Carte(int prixLunaire, int prixSolaire, String image){
+        this.prixLunaire = prixLunaire;
+        this.prixSolaire = prixSolaire;
+        this.image = image;
+    }
+}
